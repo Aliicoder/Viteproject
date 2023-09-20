2133b71f5497b2d46564d004a21a6caf13477c90
aliicoder <k.c.o.c.3000@gmail.com>
2023-09-20 10:04:16 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import keyConceptsImage from './assets/images/key-concepts.png';
+import componentsImage from './assets/images/components.png';
+import stateImage from './assets/images/state.png';
+import eventsImage from './assets/images/events.png';
+import Card from './components/Card';
+const concepts = [
+  {
+    title: 'Components',
+    image: componentsImage,
+    description:
+      'Components let you split the UI into independent, reusable pieces, and think about each piece in isolation. Components can receive data via props, and they can render dynamic output using JSX.',
+  },
+  {
+    title: 'State',
+    image: stateImage,
+    description:
+      'State is data that may change over time. As it changes, the UI should be updated to reflect the updated data. Each component can maintain its own state and multiple components can share state.',
+  },
+  {
+    title: 'Events',
+    image: eventsImage,
+    description:
+      'Event handlers are added via props to (built-in) components. You pass functions as values to such event handlers to control which functions gets executed for which event.',
+  },
+];
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <header>
+        <img src={keyConceptsImage} alt="Medal badge with a star" />
+        <h1>Key React Concepts</h1>
+        <p>Selected key React concepts you should know about</p>
+      </header>
+      <ul id="concepts">
+        <Card
+        title={concepts[0].title}
+        image={concepts[0].image}
+        description={concepts[0].description}
+        ></Card>
+        <Card
+        title={concepts[1].title}
+        image={concepts[1].image}
+        description={concepts[1].description}
+        ></Card>
+        <Card
+        title={concepts[2].title}
+        image={concepts[2].image}
+        description={concepts[2].description}
+        ></Card>
+      </ul>
+    </div>
+    </>  
+  );
 }
 
-export default App
+export default App;
